905956eadb9afac62065ce3bbee5634c7e0fea97
kruthii01 <kruthi.23cs059@sode-edu.in>
2024-10-22 15:42:01 +0530

    Runnable interface and Constructor

---
+++ K17.java
@@ -0,0 +1,30 @@
+
+package pkg1;
+class MultiThreadingFive implements Runnable{
+        int threadNumber;
+        MultiThreadingFive(int tNo){
+                this.threadNumber=tNo;
+        }
+        public void run() {
+                for(int i=1;i<6;i++) {
+                        System.out.println("Task"+i+ " by ThreadNumber "+ threadNumber);
+                        try {
+                                Thread.sleep(1000);
+                        }
+                        catch(InterruptedException e) {
+                        }
+                }
+        }
+}
+public class K17 {
+        public static void main(String[] args) {
+                for(int i=1; i<8; i++) {
+                        MultiThreadingFive mt1=new MultiThreadingFive(i);
+                        Thread t1=new Thread(mt1);
+                        t1.start();
+                }
+
+
+        }
+
+}
