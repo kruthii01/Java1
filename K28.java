a0cc9d82055e1f209bde30e55028359acffa7b5b
kruthii01 <kruthi.23cs059@sode-edu.in>
2024-11-06 20:45:31 +0530

    M7(String reverse using tools from pkg2)

---
+++ K28.java
@@ -0,0 +1,11 @@
+package pkg1;
+import pkg2.Tools3;
+public class K28 {
+	public static void main(String[] args) {
+		Tools3 t1=new Tools3();
+		System.out.println(t1.reverse("kruthi"));
+		
+		}
+
+
+}
