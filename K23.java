e4a576b32746fbb9330ae138ce26a82c50da4d70
kruthii01 <kruthi.23cs059@sode-edu.in>
2024-11-06 20:03:46 +0530

    String reverse

---
+++ K23.java
@@ -0,0 +1,17 @@
+package pkg1;
+
+public class K23 {
+
+	public static void main(String[] args) {
+		String  s1="kruthi";
+		int len1=s1.length();
+		String s2=" ";
+		for(int i=0;i<len1;i++) {
+			s2=s2+s1.substring(len1-(i+1),len1-i);
+		}
+		System.out.println(s2);
+		}
+
+	}
+
+
