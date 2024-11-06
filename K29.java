6c795481032b9b459a9f58311dd0d717159ac66b
kruthii01 <kruthi.23cs059@sode-edu.in>
2024-11-06 20:55:06 +0530

    string reverse using txt file names.txt

---
+++ K29.java
@@ -0,0 +1,26 @@
+package pkg1;
+import pkg2.Tools3;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class K29 {
+
+        public static void main(String[] args) throws FileNotFoundException {
+                File f1=new File("names.txt");
+                Scanner sc1=new Scanner(f1);
+                String name="";
+                Tools3 t1=new Tools3();
+
+                 name=sc1.nextLine();
+                System.out.println(t1.reverse(name));
+
+                name=sc1.nextLine();
+                System.out.println(t1.reverse(name));
+
+                name=sc1.nextLine();
+                System.out.println(t1.reverse(name));
+                sc1.close();
+
+        }
+
+}
